90f584245d97cad0b037b1f1afecf5c7f361b75c
durdenplato <durdenplato@gmail.com>
2017-05-01 23:16:37 +0530

    Update AlienAndPassword.java
    formatted code

--- src/com/topcoder/div2/level1/AlienAndPassword.java
+++ src/com/topcoder/div2/level1/AlienAndPassword.java
@@ -3,15 +3,15 @@ package com.topcoder.div2.level1;
 public class AlienAndPassword {
 
 	public static void main(String[] args) {
-		AlienAndPassword aap = new AlienAndPassword();
+	    AlienAndPassword aap = new AlienAndPassword();
 	    System.out.println(aap.getNumber("AGAAGAHHHHFTQLLAPUURQQRRRUFJJSBSZVJZZZ"));
 	}
-	
+
 	public int getNumber(String s){
-		int count = 0;
-		Character previousCh = null;
-		for(Character nextCh : s.toCharArray())
-			if(nextCh != previousCh) { count++; previousCh = nextCh;}
-		return count;
+	    int count = 0;
+	    Character previousCh = null;
+	    for(Character nextCh : s.toCharArray())
+		if(nextCh != previousCh) { count++; previousCh = nextCh;}
+	    return count;
 	}
 }
